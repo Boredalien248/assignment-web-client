927fec33a6e0443096e14b089fd3238923664a84
Boredalien248 <kaustubh248@gmail.com>
2023-10-10 01:55:47 -0600

    finished GET with no errors in test whatsoever

--- README.md
+++ README.md
@@ -26,3 +26,8 @@ Now everything is LICENSE'D under the Apache 2 license by Kaustubh Mukherjee
 Made by myself without the contribution of others 
 
 RESOURCES REFERENCED : 
+
+URL Parsing - https://docs.python.org/3/library/urllib.parse.html
+HTTP Codes - https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+Access Website without port number - https://stackoverflow.com/questions/54623320/how-to-use-domain-open-website-directly-without-port-number#:~:text=If%20you%20want%20to%20skip,443%20as%20per%20protocol%20use.
+Default Ports - https://www.examcollection.com/certification-training/network-plus-overview-of-common-tcp-and-udp-default-ports.html
--- httpclient.py
+++ httpclient.py
@@ -18,11 +18,13 @@
 # Write your own HTTP GET and POST
 # The point is to understand what you have to send and get experience with it
 
+# importing necessary modules 
 import sys
 import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib.parse
+
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -41,13 +43,23 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        # splits the whole response wherever there is a space and makes an array and provides the data in index 1 
+        code = data.split(" ")
+        return int(code[1])
 
     def get_headers(self,data):
-        return None
+        # splits the whole response wherever "\r\n\r\n" is present and makes an array and provides the data in index 0 as the header always comes before the first "\r\n\r\n"
+        header = data.split("\r\n\r\n")[0]
+        header += "\r\n\r\n"
+        return header
 
     def get_body(self, data):
-        return None
+        # splits the whole response wherever "\r\n\r\n" is present and makes an array and provides the data in index 1 as the body always comes after the first "\r\n\r\n"
+        body = data.split("\r\n\r\n")
+        if body is None:
+            return ''
+        else:
+            return body[1]
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -70,6 +82,67 @@ class HTTPClient(object):
     def GET(self, url, args=None):
         code = 500
         body = ""
+
+        # the formatting used 
+        FORMAT = 'utf-8'
+
+        # finding out the port, hostname and the scheme used in the request
+        port = urllib.parse.urlparse(url).port
+        host = urllib.parse.urlparse(url).hostname
+        scheme = urllib.parse.urlparse(url).scheme
+        path = urllib.parse.urlparse(url).path
+        query = urllib.parse.urlparse(url).query
+
+        # returning 400 error if port and host not mentioned 
+        if port is None and host is None:
+            return HTTPResponse(400, body)
+        
+        # setting default port if none mentioned or provided 
+        if port is None and scheme == 'https':
+            port = 443
+        if port is None and scheme == 'http':
+            port = 80
+
+        # print(path)
+        
+        # connect to the server 
+        self.connect(host, port)
+
+        # if the path is empty then use \
+        if path is None: 
+            path = '/'
+
+        # if there is a query add it with the path 
+        if query is not None:
+            path += f'?{query}'
+
+        # fomatting the request
+        request = f'GET {path} HTTP/1.1\r\nHost:{host}\r\nConnection: close\r\n\r\n'
+
+        # send the request 
+        self.sendall(request)
+
+        # receive all the data and store it in socket.
+        # response = self.socket.recv(4096)
+        response = self.recvall(self.socket)
+        response = response.encode(FORMAT)
+        response = response.decode(FORMAT)
+
+        # display the response 
+        print(response)
+
+        # get the code and body into respective variables
+        code = self.get_code(response)
+        # print(code)
+        # print('\n')
+
+        body = self.get_body(response)
+        # print(body)
+        # print('\n')
+
+        # close the connection
+        self.close()
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
